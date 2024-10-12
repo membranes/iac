d992c535b8326986d7b423eca609f30b0117a868
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-12 21:24:12 +0100

    Add src/construct.sh

---
+++ src/construct.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+: << 'comment'
+This script assumes that Amazon ECS (Elastic Container Service) Cluster the tasks depend on is active. 
+comment
+
+# Pause
+tau=2s
+
+# Path
+path=file://src
+
+# The log group of the task definition
+aws logs create-log-group --cli-input-json "$path/ecs/logs/acquire/logs.json"
+aws logs create-log-group --cli-input-json "$path/ecs/logs/prepare/logs.json"
+
+sleep $tau
+
+# The task definition vis-à-vis this repository.
+aws ecs register-task-definition --cli-input-json "$path/ecs/tasks/acquire/define.json"
+aws ecs register-task-definition --cli-input-json "$path/ecs/tasks/prepare/define.json"
+
+sleep $tau
+
+# The state machine.  It consists of two task definitions; preceding step.
+aws stepfunctions create-state-machine --cli-input-json $path/steps/define.json --definition $path/steps/machine.asl.json
