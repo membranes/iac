4059cd8f2da1c6f7975f3bf651f49eaee89d3149
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-25 20:49:53 +0100

    Update src/ec2/0001/launch.sh

--- src/ec2/0001/launch.sh
+++ src/ec2/0001/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws ec2 run-instances --user-data file://src/ec2/0001/data.txt --launch-template LaunchTemplateId={launch.template.identifier}
+aws ec2 run-instances --user-data file://src/ec2/0001/data.txt --cli-input-json $path/ec2/0001/instance.json
 
