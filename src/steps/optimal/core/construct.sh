88ad609b018f06ef9d41320a4659014c91d6e931
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-25 14:09:24 +0100

    Update src/steps/optimal/core/construct.sh

--- src/steps/optimal/core/construct.sh
+++ src/steps/optimal/core/construct.sh
@@ -4,8 +4,8 @@
 This script assumes that Amazon ECS (Elastic Container Service) Cluster the tasks depend on is active. 
 comment
 
-# Pause
-tau=2s
-
 # Path
 path=file://src
+
+# The state machine.  It consists of two task definitions; preceding step.
+aws stepfunctions create-state-machine --cli-input-json $path/steps/optimal/core/define.json --definition $path/steps/optimal/core/machine.asl.json
