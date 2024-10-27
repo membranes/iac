9cbe340460b72f97fb8e0e117c8598d51213d5be
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-27 20:47:18 +0000

    Add src/core/ec2/launch.sh

---
+++ src/core/ec2/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+<< 'comment'
+Run https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
+comment
+
+path=file://src/core/
+aws ec2 run-instances --user-data $path/ec2/data.txt --cli-input-json $path/ec2/launch.json
