f590dc438692b525bb5e826d8d8e768de4b630b9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-29 23:46:46 +0000

    Update compute/core/ecs/cluster/cluster.sh

--- compute/core/ecs/cluster/cluster.sh
+++ compute/core/ecs/cluster/cluster.sh
@@ -4,4 +4,4 @@
 Creates an Amazon Elastic Container Service (ECS) Cluster
 '
 
-aws ecs create-cluster --cli-input-json file://src/core/ecs/cluster/define.json
+aws ecs create-cluster --cli-input-json file://compute/core/ecs/cluster/define.json
