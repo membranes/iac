be5d236185e2e526bdca967be074b4526d3611b6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-31 21:46:19 +0000

    Update src/instances/0002/launch.sh

--- src/instances/0002/launch.sh
+++ src/instances/0002/launch.sh
@@ -4,6 +4,6 @@
 Run https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
 comment
 
-path=file://compute/core
+path=file://src/instances/0002
 
-aws ec2 run-instances --user-data $path/ec2/data.txt --cli-input-json $path/ec2/launch.json --region $1
+aws ec2 run-instances --user-data $path/data.txt --cli-input-json $path/launch.json --region $1
