ec25d79dc1e231d6f52fa726e2e930082cf25772
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-31 12:17:13 +0000

    Update compute/extra/steps/execute.sh

--- compute/extra/steps/execute.sh
+++ compute/extra/steps/execute.sh
@@ -4,6 +4,6 @@
 This script starts the execution of a state machine via its Amazon Resource Name (ARN).
 comment
 
-path=file://compute/core
+path=file://compute/extra
 
 aws stepfunctions start-execution --cli-input-json $path/steps/execute.json
