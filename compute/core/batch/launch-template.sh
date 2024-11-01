b874e208436e30e230f60e7216a539f8eba5331d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-01 21:13:06 +0000

    Update compute/core/batch/launch-template.sh

--- compute/core/batch/launch-template.sh
+++ compute/core/batch/launch-template.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+<< 'comment'
+https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-launch-template.html
+comment
+
 path=file://compute/core
 
 aws ec2 create-launch-template --cli-input-json $path/batch/launch-template.json --region $1
