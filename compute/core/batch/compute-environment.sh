8f21afe5f6a94cb96695dd66d0d459cc73df20fe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-01 21:12:45 +0000

    Update compute/core/batch/compute-environment.sh

--- compute/core/batch/compute-environment.sh
+++ compute/core/batch/compute-environment.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+<< 'comment'
+https://awscli.amazonaws.com/v2/documentation/api/latest/reference/batch/create-compute-environment.html
+comment
+
 path=file://compute/core
 
 aws batch create-compute-environment --cli-input-json $path/batch/compute-environment.json
