36965b4e9cc24d326b85babbd844b0fc3d7c7576
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-17 18:15:27 +0000

    Update compute/train/ec2/create-launch-template.sh

--- compute/train/ec2/create-launch-template.sh
+++ compute/train/ec2/create-launch-template.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+: << 'comment'
+This script creates the launch template of a training phase
+comment
+
 path=file://compute/train/ec2
 
 aws ec2 create-launch-template --cli-input-json "$path/define.json" --launch-template-data "$path/launch-template-data.json" --region $1
