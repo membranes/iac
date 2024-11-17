cbfb6b562e98f77a4be20144a2abe74192705338
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-17 14:45:58 +0000

    Update compute/reference/ecs/ec2/data-base64.sh

--- compute/reference/ecs/ec2/data-base64.sh
+++ compute/reference/ecs/ec2/data-base64.sh
@@ -4,4 +4,4 @@
 This script converts the user data script, data.txt, to a Base 64 format.
 comment
 
-base64 compute/extra/ec2/data.txt > compute/extra/ec2/data-base64.txt
+base64 compute/reference/ecs/ec2/data.txt > compute/reference/ecs/ec2/data-base64.txt
