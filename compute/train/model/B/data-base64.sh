a3d60540a7c76dbaed5ed20debac1f1d3c6b7513
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-17 21:16:17 +0000

    Update compute/train/model/B/data-base64.sh

--- compute/train/model/B/data-base64.sh
+++ compute/train/model/B/data-base64.sh
@@ -4,4 +4,4 @@
 This script converts the user data script, data.txt, to a Base 64 format.
 comment
 
-base64 compute/train/ec2/data.txt > compute/train/ec2/data-base64.txt
+base64 compute/train/model/B/data.txt > compute/train/model/B/data-base64.txt
