1bf2300e1c5ea902bf9d1e621d4341557977bf49
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-17 21:19:13 +0000

    Delete compute/train/model/B/data-base64.sh

--- compute/train/model/B/data-base64.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-: << 'comment'
-This script converts the user data script, data.txt, to a Base 64 format.
-comment
-
-base64 compute/train/model/B/data.txt > compute/train/model/B/data-base64.txt
