ea8c7dba477d5b28e8b00213db6f3f9c3ebac983
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-20 22:49:48 +0000

    Update src/ecs/ec2/create-launch-template.sh

--- src/ecs/ec2/create-launch-template.sh
+++ src/ecs/ec2/create-launch-template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-path=file://compute/reference/ecs/ec2
+path=file://src/ecs/ec2
 
 aws ec2 create-launch-template --cli-input-json "$path/define.json" --launch-template-data "$path/launch-template-data.json" --region $1
