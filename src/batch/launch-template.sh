57ddcb39709829130bde83c5bba36db8318b5cad
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-18 17:57:33 +0000

    Delete src/batch/launch-template.sh

--- src/batch/launch-template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-<< 'comment'
-https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-launch-template.html
-comment
-
-path=file://compute/core/batch
-
-aws ec2 create-launch-template --cli-input-json $path/launch-template.json --region $1
