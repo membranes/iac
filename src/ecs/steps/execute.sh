bfc73ade26bd421c7f11e94bb9f76e5b8340018f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-18 17:33:16 +0000

    Delete src/ecs/steps/execute.sh

--- src/ecs/steps/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-: << 'comment'
-This script starts the execution of a state machine via its Amazon Resource Name (ARN).
-comment
-
-path=file://compute/reference
-
-aws stepfunctions start-execution --cli-input-json $path/steps/execute.json
