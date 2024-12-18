4731e46fea497c5bb2a72dccc1482fdce012d47b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-18 18:29:19 +0000

    Update compute/train/run.sh

--- compute/train/run.sh
+++ compute/train/run.sh
@@ -3,8 +3,10 @@
 << 'comment'
 Notes:
 * https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
+* $1: A path, i.e., model architecture name, parameter.
+* $2: An Amazon region code parameter.
 comment
 
 path=file://compute/train
 
-aws ec2 run-instances --cli-input-json "$path/run.json" --user-data "$path/data.txt" --region $1
+aws ec2 run-instances --cli-input-json "$path/run.json" --user-data "$path/$1/data.txt" --region $2
